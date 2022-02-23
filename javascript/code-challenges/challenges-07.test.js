49c592adc0348f6efca96240f085881795320c96
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-22 17:33:11 -0800

    cc4 passes test

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -65,6 +65,10 @@ For example, if the input is 'Welcome', the output will be:
 const howMuchPencil = (str) => {
   let result = [];
   // Solution code here...
+  for(let i=0; i <= str.length; i++){
+    result.push(str.slice(i));
+  }
+
   return result;
 };
 
@@ -76,9 +80,7 @@ Write a function name wordsToCharList that, given a string as input, returns a n
 For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
-const wordsToCharList = (arr) => {
-  // Solution code here...
-};
+const wordsToCharList = (arr) => arr.split('');
 
 
 /* ------------------------------------------------------------------------------------------------
