fb2c108fd4b98b0f579e98f598d1f2b2f7ac0350
MichaelCampbell2020 <cam3000@gmail.com>
2022-02-25 19:18:39 -0800

    1-5 work

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -99,8 +99,16 @@ Write a function named salesData that uses forEach to iterate over the hourlySal
 
 const salesData = (hours, data) => {
   // Solution code here...
-};
+  let soldCookies = [];
 
+  for (let i = 0; i < hours.length; i++) {
+    soldCookies.push({
+      sales: `${data[i]} cookies`,
+      time: hours[i]
+    })
+  }
+  return soldCookies;
+};
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
 
